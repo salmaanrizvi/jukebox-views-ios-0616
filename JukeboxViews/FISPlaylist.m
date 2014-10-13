2697f736408e0f9e372972a499384bd21e6211ea
Chris Gonzales <gonzales.chris.a@gmail.com>
2014-10-13 07:11:35 -0400

    cleanup- update solution

--- JukeboxViews/FISPlaylist.m
+++ JukeboxViews/FISPlaylist.m
@@ -10,19 +10,30 @@
 
 @implementation FISPlaylist
 
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        _songs = [[NSMutableArray alloc] init];
+    }
+    return self;
+}
+
 -(NSMutableArray *)songs
 {
-    if (!_songs) {
-        
-        FISSong *song1 = [[FISSong alloc] initWithTitle:@"Hold on Be Strong" artist:@"Matoma vs Big Poppa" album:@"The Internet 1" andFileName:@"hold_on_be_strong"];
-        FISSong *song2 = [[FISSong alloc] initWithTitle:@"Higher Love" artist:@"Matoma ft. James Vincent McMorrow" album:@"The Internet 2" andFileName:@"higher_love"];
-        FISSong *song3 = [[FISSong alloc] initWithTitle:@"Mo Money Mo Problems" artist:@"Matoma ft. The Notorious B.I.G." album:@"The Internet 3" andFileName:@"mo_money_mo_problems"];
-        FISSong *song4 = [[FISSong alloc] initWithTitle:@"Old Thing Back" artist:@"The Notorious B.I.G." album:@"The Internet 4" andFileName:@"old_thing_back"];
-        FISSong *song5 = [[FISSong alloc] initWithTitle:@"Gangsta Bleeding Love" artist:@"Matoma" album:@"The Internet 5" andFileName:@"gangsta_bleeding_love"];
-        FISSong *song6 = [[FISSong alloc] initWithTitle:@"Bailando" artist:@"Enrique Iglesias ft. Sean Paul" album:@"The Internet 6" andFileName:@"bailando"];
-        
-        _songs = [[NSMutableArray alloc] initWithArray:@[song1, song2, song3, song4, song5, song6]];
-    }
+//    uncomment below if you just want to run the jukebox (not testing) and have the included songs set up automatically
+    
+//    if (!_songs) {
+//        
+//        FISSong *song1 = [[FISSong alloc] initWithTitle:@"Hold on Be Strong" artist:@"Matoma vs Big Poppa" album:@"The Internet 1" andFileName:@"hold_on_be_strong"];
+//        FISSong *song2 = [[FISSong alloc] initWithTitle:@"Higher Love" artist:@"Matoma ft. James Vincent McMorrow" album:@"The Internet 2" andFileName:@"higher_love"];
+//        FISSong *song3 = [[FISSong alloc] initWithTitle:@"Mo Money Mo Problems" artist:@"Matoma ft. The Notorious B.I.G." album:@"The Internet 3" andFileName:@"mo_money_mo_problems"];
+//        FISSong *song4 = [[FISSong alloc] initWithTitle:@"Old Thing Back" artist:@"The Notorious B.I.G." album:@"The Internet 4" andFileName:@"old_thing_back"];
+//        FISSong *song5 = [[FISSong alloc] initWithTitle:@"Gangsta Bleeding Love" artist:@"Matoma" album:@"The Internet 5" andFileName:@"gangsta_bleeding_love"];
+//        FISSong *song6 = [[FISSong alloc] initWithTitle:@"Bailando" artist:@"Enrique Iglesias ft. Sean Paul" album:@"The Internet 6" andFileName:@"bailando"];
+//        
+//        _songs = [[NSMutableArray alloc] initWithArray:@[song1, song2, song3, song4, song5, song6]];
+//    }
     
     return _songs;
 }
@@ -41,23 +52,28 @@
 
 -(void)sortSongsByTitle
 {
-    NSSortDescriptor *descriptor = [[NSSortDescriptor alloc] initWithKey:@"title" ascending:YES];
-    NSArray *descriptors = @[descriptor];
+    NSSortDescriptor *descriptor1 = [[NSSortDescriptor alloc] initWithKey:@"title" ascending:YES];
+    NSSortDescriptor *descriptor2 = [[NSSortDescriptor alloc] initWithKey:@"artist" ascending:YES];
+    NSArray *descriptors = @[descriptor1, descriptor2];
     self.songs = [[NSMutableArray alloc] initWithArray:[self.songs sortedArrayUsingDescriptors:descriptors]];
     
 }
 
 -(void)sortSongsByArtist
 {
-    NSSortDescriptor *descriptor = [[NSSortDescriptor alloc] initWithKey:@"artist" ascending:YES];
-    NSArray *descriptors = @[descriptor];
+    NSSortDescriptor *descriptor1 = [[NSSortDescriptor alloc] initWithKey:@"artist" ascending:YES];
+    NSSortDescriptor *descriptor2 = [[NSSortDescriptor alloc] initWithKey:@"album" ascending:YES];
+    NSSortDescriptor *descriptor3 = [[NSSortDescriptor alloc] initWithKey:@"title" ascending:YES];
+    
+    NSArray *descriptors = @[descriptor1, descriptor2, descriptor3];
     self.songs = [[NSMutableArray alloc] initWithArray:[self.songs sortedArrayUsingDescriptors:descriptors]];
 }
 
 -(void)sortSongsByAlbum
 {
-    NSSortDescriptor *descriptor = [[NSSortDescriptor alloc] initWithKey:@"album" ascending:YES];
-    NSArray *descriptors = @[descriptor];
+    NSSortDescriptor *descriptor1 = [[NSSortDescriptor alloc] initWithKey:@"album" ascending:YES];
+    NSSortDescriptor *descriptor2 = [[NSSortDescriptor alloc] initWithKey:@"title" ascending:YES];
+    NSArray *descriptors = @[descriptor1, descriptor2];
     self.songs = [[NSMutableArray alloc] initWithArray:[self.songs sortedArrayUsingDescriptors:descriptors]];
 }
 
